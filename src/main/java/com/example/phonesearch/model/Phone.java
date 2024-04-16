6f0f761ea138dfb47df0a3c223b852af092561a4
mikhi-mh <hmikhilesh@gmail.com>
2024-04-16 22:30:38 +0530

    corrected data-types

--- src/main/java/com/example/phonesearch/model/Phone.java
+++ src/main/java/com/example/phonesearch/model/Phone.java
@@ -2,35 +2,41 @@ package com.example.phonesearch.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(schema = "phones")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Phone {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String brand_name;
     private String model;
-    private double price;
-    private double avg_rating;
-    private boolean is_5G;
+    private Double price;
+    private Double avg_rating;
+    private Boolean is_5G;
     private String processor_brand;
-    private int num_cores;
-    private double processor_speed;
-    private int battery_capacity;
-    private boolean fast_charging_available;
-    private String fast_charging;
-    private int ram_capacity;
-    private int internal_memory;
-    private double screen_size;
-    private int refresh_rate;
-    private int num_rear_cameras;
+    private Integer num_cores;
+    private Double processor_speed;
+    private Integer battery_capacity;
+    private Boolean fast_charging_available;
+    private Integer fast_charging;
+    private Integer ram_capacity;
+    private Integer internal_memory;
+    private Double screen_size;
+    private Integer refresh_rate;
+    private Integer num_rear_cameras;
     private String os;
-    private String primary_camera_rear;
-    private String primary_camera_front;
-    private boolean extended_memory_available;
-    private int resolution_height;
-    private int resolution_width;
+    private Double primary_camera_rear;
+    private Double primary_camera_front;
+    private Boolean extended_memory_available;
+    private Integer resolution_height;
+    private Integer resolution_width;
 
 }
